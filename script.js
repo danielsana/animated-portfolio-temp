bfeedc12b1d41991fc3306819b41655f1f04fd1a
dansantos2 <danielsana60@gmail.com>
2023-07-12 13:17:34 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "UX & UI Designer", "Tech Tutor"], typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
